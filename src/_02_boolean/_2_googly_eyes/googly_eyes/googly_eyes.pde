2dc94be0ce25ce71e17755068484f4993db2f448
root <root@1cd3db076f89>
2020-10-26 22:32:15 +0000

    started pi aloud and googly eyes, finished string methods

--- src/_02_boolean/_2_googly_eyes/googly_eyes/googly_eyes.pde
+++ src/_02_boolean/_2_googly_eyes/googly_eyes/googly_eyes.pde
@@ -1,8 +1,12 @@
 
+
+PImage spongebob;
 void setup() {
-  
+ spongebob = loadImage("spongebob.jpg");
+    size(800,600);
+    spongebob.resize(800,500);
 }
 
 void draw() {
-  
+  print (mouseX+" "+mouseY);
 }
--- src/_03_char/_3_pi_aloud/PiAloud.java
+++ src/_03_char/_3_pi_aloud/PiAloud.java
@@ -3,7 +3,12 @@ package _03_char._3_pi_aloud;
 import java.io.IOException;
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class PiAloud {
+	public static void main(String[] args) {
+		
+	
 	/***********  SOUND ***************
 	 * Some computers are unable to play sounds. 
 	 * If you cannot play sound on this computer, set canPlaySounds to false.
@@ -15,7 +20,7 @@ public class PiAloud {
 	// 1. Make a main method and make sure your program can run
 
 	// 2. Make a String variable to hold 20 digits of Pi. You could use http://www.piday.org/million/ to get this.
-
+String pi = JOptionPane.showInputDialog()
 	// 3. Print out 3 digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
 	// Check the console shows 3.1
 
@@ -35,7 +40,7 @@ public class PiAloud {
 	
 	
 	/********************  Use these methods. DON'T CHANGE THE CODE BELOW  ******************/
-
+	}
 	static void speak(char characterToSpeak) {
 		
 		if (System.getProperty("os.name").contains("Windows")) {
