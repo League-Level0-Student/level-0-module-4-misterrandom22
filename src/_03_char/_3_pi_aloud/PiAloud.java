78c0c76848fadcc3cbe0fc2c36c06432e8bfe0af
root <root@d268793a7863>
2020-11-09 23:33:56 +0000

    finished pi aloud.

--- src/_03_char/_3_pi_aloud/PiAloud.java
+++ src/_03_char/_3_pi_aloud/PiAloud.java
@@ -7,37 +7,55 @@ import javax.swing.JOptionPane;
 
 public class PiAloud {
 	public static void main(String[] args) {
-		
-	
-	/***********  SOUND ***************
-	 * Some computers are unable to play sounds. 
-	 * If you cannot play sound on this computer, set canPlaySounds to false.
-	 * If you are not sure, ask your teacher 
-	 * *****************/
-	boolean canPlaySounds = true;
-	
-	
-	// 1. Make a main method and make sure your program can run
 
-	// 2. Make a String variable to hold 20 digits of Pi. You could use http://www.piday.org/million/ to get this.
-String pi = JOptionPane.showInputDialog()
-	// 3. Print out 3 digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
-	// Check the console shows 3.1
 
-	// 4. Print ALL the digits of the Pi String, putting each digit on a new line  (hint: use a loop)
+		/***********  SOUND ***************
+		 * Some computers are unable to play sounds. 
+		 * If you cannot play sound on this computer, set canPlaySounds to false.
+		 * If you are not sure, ask your teacher 
+		 * *****************/
+		boolean canPlaySounds = true;
 
-	// 5. Skip this step if your computer cannot play sounds. 
-	//		if (canPlaySounds) {
-	//				 Use the speak() method to speak each digit of Pi.   
-	//		}
-	
-	
-	// [CHALLENGE]
-	// *6. Get a character from the user using the getInputFromUser() method
-	// *7. Compare the users' char to the next digit of Pi
-	// *8. If they are correct, print out "correct". If they are not, print "incorrect" 
-	
-	
+
+		// 1. Make a main method and make sure your program can run
+
+		// 2. Make a String variable to hold 20 digits of Pi. You could use http://www.piday.org/million/ to get this.
+		String pi = ("3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342");
+		// 3. Print out 3 digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
+		// Check the console shows 3.1
+		System.out.println(""+pi.charAt(0)+pi.charAt(1)+pi.charAt(2)+pi.charAt(3));
+
+		// 4. Print ALL the digits of the Pi String, putting each digit on a new line  (hint: use a loop)
+		for (int i = 0; i<pi.length(); i++) {
+			System.out.println(pi.charAt(i));
+
+
+			// 5. Skip this step if your computer cannot play sounds. 
+			//		if (canPlaySounds) {
+			//				 Use the speak() method to speak each digit of Pi.   
+			//		}
+
+
+			// [CHALLENGE]
+			// *6. Get a character from the user using the getInputFromUser() method
+			String input = JOptionPane.showInputDialog("enter the next character of pi");
+			char number = input.charAt(0);
+			// *7. Compare the users' char to the next digit of Pi
+			if (number == (pi.charAt(i))) {
+
+				System.out.println("correct");
+
+			}
+
+			else{
+
+				System.out.println("incorrect");	
+			}
+
+
+			// *8. If they are correct, print out "correct". If they are not, print "incorrect" 
+		}
+
 	
 	/********************  Use these methods. DON'T CHANGE THE CODE BELOW  ******************/
 	}
